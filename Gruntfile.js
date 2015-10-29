0b37f3364717ac2ba85bcbd1029a59bbc5c6eee0
Alejandro DE MARIA <ademaria@pc593.embl.fr>
2015-10-29 10:48:11 +0100

    Moved bower_components to root folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,8 +14,8 @@ module.exports = function(grunt) {
 								   "js/main/managerwelcomemainview.js", "js/main/shippingmainview.js", "js/main/sessionmainview.js", "js/main/saxs/landing/*.js", 
 						  		   "js/main/saxs/hplc/*.js",   "js/main/landing/*.js", "js/main/saxs/prepare/*.js", "js/main/saxs/*.js", "js/main/mx/*.js", 
 					                           "js/main/mx/*/*.js", "js/main/tool/*.js", "js/main/run/*.js"],
-				 'min/bower_components.min.js' 	: ["js/bower_components/dygraphs/dygraph-combined.js", "js/bower_components/handsontable/dist/handsontable.full.js","js/bower_components/jquery/dist/jquery.min.js",
-								   "js/bower_components/jquery-lazy/jquery.lazy.js", "js/bower_components/moment/min/moment.min.js"
+				 'min/bower_components.min.js' 	: ["bower_components/dygraphs/dygraph-combined.js", "bower_components/handsontable/dist/handsontable.full.js","bower_components/jquery/dist/jquery.min.js",
+								   "bower_components/jquery-lazy/jquery.lazy.js", "bower_components/moment/min/moment.min.js"
 								],
 				 'min/dependency.min.js' 	: ["js/dependency/three49custom.js", "js/dependency/glmol.js","js/dependency/timeline/timeline.js", "js/dependency/pathjs/path.js"]
 			  }
@@ -48,7 +48,7 @@ module.exports = function(grunt) {
 			      roundingPrecision: -1
 			  },
 			    files: {
-			          'min/exi.min.css': [ 'css/dygraph-custom.css', 'css/exi.css', 'css/calendar.css', 'css/menu/mainmenu.css', 'css/override.css', 'js/bower_components/handsontable/dist/handsontable.full.css', 'js/dependency/timeline/timeline.css']
+			          'min/exi.min.css': [ 'css/dygraph-custom.css', 'css/exi.css', 'css/calendar.css', 'css/menu/mainmenu.css', 'css/override.css', 'bower_components/handsontable/dist/handsontable.full.css', 'js/dependency/timeline/timeline.css']
 			    }
 		}
 	}
