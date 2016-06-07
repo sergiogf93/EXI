081cc12bee3ae1c8d9a47b9fbc7db88ef3bf8d0d
Alejandro De Maria Antolinos <demariaa@lindemaria.esrf.fr>
2016-06-07 10:52:39 +0200

    Plots for autoprocessing showed as not linear but single x-point

--- js/widget/plot/curveplotter.js
+++ js/widget/plot/curveplotter.js
@@ -1,115 +1,115 @@
 function CurvePlotter(args) {
-	this.id = BUI.id();
+    this.id = BUI.id();
 
-	this.backgroundColor = "#FFFFFF";
-	
-	this.margin = '0 0 0 5';
-	this.ruleColor ="black";
-	this.targetId = "plotCanvas" + BUI.id();
-	this.legend = 'onmouseover';
-	
-	if (args != null){
-		if (args.margin != null){
-			this.margin = args.margin;
-		}
-		if (args.legend != null){
-			this.legend = args.legend;
-		}
-	}
-	
-	this.onRendered = new Event(this);
-	this.onPointClickCallback = new Event();
-     
+    this.backgroundColor = "#FFFFFF";
+
+    this.margin = '0 0 0 5';
+    this.ruleColor = "black";
+    this.targetId = "plotCanvas" + BUI.id();
+    this.legend = 'onmouseover';
+
+    if (args != null) {
+        if (args.margin != null) {
+            this.margin = args.margin;
+        }
+        if (args.legend != null) {
+            this.legend = args.legend;
+        }
+    }
+
+    this.onRendered = new Event(this);
+    this.onPointClickCallback = new Event();
+
 }
 
 CurvePlotter.prototype.getPanel = function() {
-	this.plotPanel = Ext.create('Ext.container.Container', {
-	    layout: {
-	        type: 'hbox'
-	    },
-	    flex : 0.7,
-	    margin : this.margin,
-	    items: [{
-	    	html : '<div id="' + this.targetId +'"></div>',
-	    	id : this.id,
-	    }]
-	});
-	
-	this.plotPanel.on("afterrender", function(){
-	});
-	
-	this.plotPanel.on("resize", function(){
-	});
-	return this.plotPanel;
+    this.plotPanel = Ext.create('Ext.container.Container', {
+        layout: {
+            type: 'hbox'
+        },
+        flex: 0.7,
+        margin: this.margin,
+        items: [{
+            html: '<div id="' + this.targetId + '"></div>',
+            id: this.id,
+        }]
+    });
+
+    this.plotPanel.on("afterrender", function() {
+    });
+
+    this.plotPanel.on("resize", function() {
+    });
+    return this.plotPanel;
 };
 
 CurvePlotter.prototype.getPointCount = function() {
-	return this.dygraph.rawData_.length;
+    return this.dygraph.rawData_.length;
 };
 
 CurvePlotter.prototype.getColors = function() {
-	return this.dygraph.getColors();
+    return this.dygraph.getColors();
 };
 
 CurvePlotter.prototype.getLabels = function() {
-	return this.dygraph.getLabels();
+    return this.dygraph.getLabels();
 };
 
 CurvePlotter.prototype.render = function(url) {
-	var _this = this;
-	if (document.getElementById(this.targetId) != null){
-		document.getElementById(this.targetId).innerHTML = "";
-	
-		this.width = this.plotPanel.getWidth();
-		this.height = this.plotPanel.getHeight();
-		
-		document.getElementById(this.targetId).setAttribute("style", "border: 1px solid #000000; height:" + (this.plotPanel.getHeight() - 1) + "px;width:" + (this.plotPanel.getWidth() - 2) + "px;");
-		
-		Ext.getCmp(this.id).setHeight(this.plotPanel.getHeight());
-		Ext.getCmp(this.id).setWidth(this.plotPanel.getWidth());
-		
-		
-		this.dygraph = new Dygraph(
-			      document.getElementById(this.targetId),
-			      url,
-			      {
-			    	  title : this.title,
-			    	  titleHeight : 20,
-                      dateWindow: this.dateWindow,
-			    	  legend: this.legend,
-			    	  labelsSeparateLines : true,
-			          errorBars: true,
-			          connectSeparatedPoints: true,
-			          pointClickCallback: function(e, p) {
-			        	  _this.onPointClickCallback.notify(p.name);
-			          }
-			      }
-	
-			 );
-		
-		var _this = this;
-		this.dygraph.ready(function() {
-			_this.onRendered.notify();
-		});
-	
-	}
+    var _this = this;
+    if (document.getElementById(this.targetId) != null) {
+        document.getElementById(this.targetId).innerHTML = "";
+
+        this.width = this.plotPanel.getWidth();
+        this.height = this.plotPanel.getHeight();
+
+        document.getElementById(this.targetId).setAttribute("style", "border: 1px solid #000000; height:" + (this.plotPanel.getHeight() - 1) + "px;width:" + (this.plotPanel.getWidth() - 2) + "px;");
+
+        Ext.getCmp(this.id).setHeight(this.plotPanel.getHeight());
+        Ext.getCmp(this.id).setWidth(this.plotPanel.getWidth());
+
+
+        this.dygraph = new Dygraph(
+            document.getElementById(this.targetId),
+            url,
+            {
+                title: this.title,
+                titleHeight: 20,
+
+                legend: this.legend,
+                labelsSeparateLines: true,
+                errorBars: true,
+                connectSeparatedPoints: true,
+                pointClickCallback: function(e, p) {
+                    _this.onPointClickCallback.notify(p.name);
+                }
+            }
+
+        );
+
+        var _this = this;
+        this.dygraph.ready(function() {
+            _this.onRendered.notify();
+        });
+
+    }
 };
 
 CurvePlotter.prototype.loadMerge = function(subtractionIdList, from, to, scale) {
-	this.render(EXI.getDataAdapter().saxs.hplc.getFramesMergeURL(subtractionIdList, from, to, scale));
+    this.render(EXI.getDataAdapter().saxs.hplc.getFramesMergeURL(subtractionIdList, from, to, scale));
 };
 
 CurvePlotter.prototype.loadHPLCFrame = function(experimentId, frameNumber) {
-	this.render(EXI.getDataAdapter().saxs.hplc.getHPLCFramesScatteringURL(experimentId, frameNumber));
+    this.render(EXI.getDataAdapter().saxs.hplc.getHPLCFramesScatteringURL(experimentId, frameNumber));
 };
 
 CurvePlotter.prototype.loadUrl = function(url) {
-	this.render(url);
+    this.render(url);
 };
 
 
 CurvePlotter.prototype.load = function(selections) {
-	this.render(EXI.getDataAdapter().saxs.frame.getFramesURL(selections.frame, selections.average, selections.subtracted, selections.sampleaverage, selections.bufferaverage));
+    this.render(EXI.getDataAdapter().saxs.frame.getFramesURL(selections.frame, selections.average, selections.subtracted, selections.sampleaverage, selections.bufferaverage));
 };
 
 
@@ -119,44 +119,211 @@ CurvePlotter.prototype.load = function(selections) {
 
 
 function AutoProcIntegrationCurvePlotter(args) {
-	CurvePlotter.call(this, args);
-	this.dateWindow = [6, 0];
-	this.margin = '10 0 0 0';
-	this.height = null;
-	this.title = "";
-	if (args != null){
-		if (args.height != null){
-			this.height = args.height;
-		}
-		if (args.title != null){
-			this.title = args.title;  
-		}
-	}
+    CurvePlotter.call(this, args);
+
+    this.margin = '10 0 0 0';
+    this.height = null;
+    this.title = "";
+    if (args != null) {
+        if (args.height != null) {
+            this.height = args.height;
+        }
+        if (args.title != null) {
+            this.title = args.title;
+        }
+    }
+
+    this.data = {
+        labels: [], // labels = [{name: 'axisX', x: true, y, false},{name: 'axisXY', x: false, y, true}] 
+        data: []
+    };
+
+    this.xLabels = [];
 }
 
 
 AutoProcIntegrationCurvePlotter.prototype.getPointCount = CurvePlotter.prototype.getPointCount;
 AutoProcIntegrationCurvePlotter.prototype.getLabels = CurvePlotter.prototype.getLabels;
-AutoProcIntegrationCurvePlotter.prototype.render = CurvePlotter.prototype.render;
-AutoProcIntegrationCurvePlotter.prototype.loadUrl = CurvePlotter.prototype.loadUrl;
+
+
+AutoProcIntegrationCurvePlotter.prototype.toCSV = function(labels, data) {
+    var csv = labels.toString() + "\n";
+    for (var i = 0; i< data.length; i++){
+        for (var j = 0; j< data[i].length; j++){
+            csv = csv +  data[i][j] + "," ;
+        }
+        /** Removing last , */
+        csv = csv.substring(0, csv.length - 1);
+        csv = csv + "\n";
+        
+    }
+    return csv;
+};
+/**
+* Render the dygraph widget on a container that should exists with id = this.id
+*
+* 
+* @method render
+*/
+AutoProcIntegrationCurvePlotter.prototype.render = function(labels, data) {
+    var _this = this;
+
+    
+    /** Plotting */
+    var g = new Dygraph(
+        document.getElementById(this.targetId),
+        this.toCSV(labels, data),
+        {
+            title: this.title,
+            titleHeight: 20,
+          
+            height: this.height,
+            legend: this.legend,
+            labelsSeparateLines: true,
+            axisLabelWidth : 20,
+            connectSeparatedPoints: true,
+            pointClickCallback: function(e, p) {
+                _this.onPointClickCallback.notify(p.name);
+            },
+            axes: {
+                x: {
+                    axisLabelFormatter: function(d, gran, opts) {
+                        return _this.xLabels[d];                        
+                    }
+                }
+            }
+        }
+
+    );
+
+    g.ready(function() {
+        //g.setAnnotations(_this.getAnnotations(data, labels));
+    });
+
+};
+
+/**
+ * Example csv
+Resolution,11259175,11259180,11259326
+2.4,143.9,0,143.9,0,,0
+2.57,99.2,0,99.2,0,,0
+2.77,62.7,0,62.7,0,,0
+3.04,41.4,0,41.4,0,,0
+3.39,24.0,0,24.0,0,,0
+3.74,,0,,0,56.0,0
+3.87,,0,,0,55.0,0
+3.91,18.2,0,18.2,0,,0
+4.03,,0,,0,53.0,0
+4.21,,0,,0,53.6,0
+4.43,,0,,0,54.6,0
+4.71,,0,,0,53.9,0
+4.78,19.1,0,19.1,0,,0
+5.07,,0,,0,49.2,0
+5.59,,0,,0,52.2,0
+6.39,,0,,0,45.5,0
+6.73,16.4,0,16.4,0,,0
+8.05,,0,,0,38.1,0
+ */
+AutoProcIntegrationCurvePlotter.prototype.loadUrl = function(url) {
+
+    var _this = this;
+    $.ajax({
+        url: url,
+        context: this
+
+    }).done(function(csv) {
+        var index = 0;
+        var _this = this;
+        _this.xLabels = [];
+        if (csv) {
+            var lines = csv.split("\n");
+            var labelsHeader = [];
+
+            if (lines) {
+                if (lines[0]) {
+                    this.data.labels = lines[0].split(",");
+
+                }
+                else {
+                    /** No Lines */
+                    EXI.setError("No labels on csv");
+                    return;
+                }
+
+                function convertToNumber(element) {
+                    var noError = [];
+
+                    var elements = element.split(',');
+                    function toNumber(el) {
+                        if (el) {
+                            if (el != "") {
+                                return parseFloat(el);
+                            }
+                            else {
+                                return "";
+                            }
+                        }
+                        else {
+                            return "";
+                        }
+                    }
+                    elements = _.map(elements, toNumber);
+
+
+                    /** Removing the errors */
+                    var noError = [];
+                    //noError.push(elements[0]);
+                    noError.push(index);
+                    _this.xLabels.push(elements[0]);
+
+                    for (var i = 1; i < elements.length; i++) {
+                        if (i % 2 != 0) {
+                            noError.push(elements[i]);
+                        }
+                    }
+                    index = index + 1;
+                    return noError;
+                }
+
+                /** Parsing data it means remove labels, split by , and convert to number */
+                this.data.data = _.map(_.slice(lines, 1, lines.length - 1), convertToNumber);
+
+
+                try {
+
+                    this.render(this.data.labels, this.data.data);
+                }
+                catch (e) {
+                    EXI.setError(e.message);
+                }
+            }
+        }
+        else {
+            /** No Lines */
+            EXI.setError("CSV is empty");
+            return;
+        }
+    });
+
+};
 
 AutoProcIntegrationCurvePlotter.prototype.getPanel = function() {
-	this.plotPanel = Ext.create('Ext.panel.Panel', {
-	    layout: {
-	        type: 'fit'
-	    },
-	    height : this.height,
-	    margin : this.margin,
-	    items: [{
-	    	html : '<div id="' + this.targetId +'"></div>',
-	    	id : this.id,
-	    }]
-	});
-	
-	this.plotPanel.on("afterrender", function(){
-	});
-	
-	this.plotPanel.on("resize", function(){
-	});
-	return this.plotPanel;
+    this.plotPanel = Ext.create('Ext.panel.Panel', {
+        layout: {
+            type: 'fit'
+        },
+        height: this.height,
+        margin: this.margin,
+        items: [{
+            html: '<div id="' + this.targetId + '"></div>',
+            id: this.id,
+        }]
+    });
+
+    this.plotPanel.on("afterrender", function() {
+    });
+
+    this.plotPanel.on("resize", function() {
+    });
+    return this.plotPanel;
 };
