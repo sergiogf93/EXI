b233fbb5fa2978cdba9c744f93d7af89dee5604d
Alejandro De Maria Antolinos <demariaa@lindemaria.esrf.fr>
2017-01-31 13:45:17 +0100

    It fixes #59 and #60

--- templates/mx/datacollectionsummary/completeness.autoproc.mxdatacollectiongrid.template.js
+++ templates/mx/datacollectionsummary/completeness.autoproc.mxdatacollectiongrid.template.js
@@ -11,10 +11,19 @@
             <td valign="top">Inner</td>
             <td>
                     <div class="progress">
-                        <div class="progress-bar progress-bar"  role="progressbar" aria-valuenow="{.innerShell.completeness}" aria-valuemin="0" aria-valuemax="100" style="width:{.innerShell.completeness}%">
+
+                    {@gte key=innerShell.completeness value=50}
+                        {@gte key=innerShell.completeness value=90}                            
+                            <div class="progress-bar progress-bar"  role="progressbar" aria-valuenow="{.innerShell.completeness}" aria-valuemin="0" aria-valuemax="100" style="width:{.innerShell.completeness}%">{.innerShell.completeness}%</div>
+                        {:else}
+                            <div class="progress-bar progress-bar"  role="progressbar" aria-valuenow="{.innerShell.completeness}" aria-valuemin="0" aria-valuemax="100" style="background-color:#f0ad4e;width:{.innerShell.completeness}%">{.innerShell.completeness}%</div>
+                        {/gte}
+                    {:else}    
+                        <div class="progress-bar progress-bar"  role="progressbar" aria-valuenow="{.innerShell.completeness}" aria-valuemin="0" aria-valuemax="100" style="background-color:#d9534f;width:{.innerShell.completeness}%">{.innerShell.completeness}%</div>
+                    {/gte}
+
+
                         
-                        {.innerShell.completeness}%
-                        </div>
                     </div>
             </td>
             <td valign="top" style='padding:0 15px 0 15px;'>{.innerShell.resolutionsLimitHigh}</td>
@@ -24,9 +33,19 @@
             <td valign="top">Outer</td>
             <td>
                     <div class="progress">
-                        <div class="progress-bar progress-bar"   role="progressbar" aria-valuenow="{.outerShell.completeness}" aria-valuemin="0" aria-valuemax="100" style="width:{.outerShell.completeness}%">
-                        {.outerShell.completeness}%
-                        </div>
+
+                     {@gte key=outerShell.completeness value=50}
+                        {@gte key=outerShell.completeness value=90}                            
+                        <div class="progress-bar progress-bar"   role="progressbar" aria-valuenow="{.outerShell.completeness}" aria-valuemin="0" aria-valuemax="100" style="width:{.outerShell.completeness}%">{.outerShell.completeness}%</div>
+                        {:else}
+                            <div class="progress-bar progress-bar"   role="progressbar" aria-valuenow="{.outerShell.completeness}" aria-valuemin="0" aria-valuemax="100" style="background-color:#f0ad4e;width:{.outerShell.completeness}%">{.outerShell.completeness}%</div>
+                        {/gte}
+                    {:else}    
+                        <div class="progress-bar progress-bar"   role="progressbar" aria-valuenow="{.outerShell.completeness}" aria-valuemin="0" aria-valuemax="100" style="background-color:#d9534f;width:{.outerShell.completeness}%">{.outerShell.completeness}%</div>
+                    {/gte}
+
+
+                        
                     </div>
             </td>
             <td valign="top" style='padding:0 15px 0 15px;'>{.outerShell.resolutionsLimitHigh}</td>
@@ -36,9 +55,17 @@
             <td valign="top">Overall</td>
             <td>
                     <div class="progress">
-                        <div class="progress-bar progress-bar"   role="progressbar" aria-valuenow="{.overall.completeness}" aria-valuemin="0" aria-valuemax="100" style="width:{.overall.completeness}%">
-                        {.overall.completeness}%
-                        </div>
+                            {@gte key=overall.completeness value=50}
+                                {@gte key=overall.completeness value=90}                            
+                                <div class="progress-bar progress-bar"   role="progressbar" aria-valuenow="{.overall.completeness}" aria-valuemin="0" aria-valuemax="100" style="width:{.overall.completeness}%">{.overall.completeness}%</div>
+                                {:else}
+                                    <div class="progress-bar progress-bar"   role="progressbar" aria-valuenow="{.overall.completeness}" aria-valuemin="0" aria-valuemax="100" style="background-color:#f0ad4e;width:{.overall.completeness}%">{.overall.completeness}%</div>
+                                {/gte}
+                            {:else}    
+                                <div class="progress-bar progress-bar"   role="progressbar" aria-valuenow="{.overall.completeness}" aria-valuemin="0" aria-valuemax="100" style="background-color:#d9534f;width:{.overall.completeness}%">{.overall.completeness}%</div>
+                            {/gte}
+
+                        
                     </div>
             </td>
             <td valign="top"  style='padding:0 15px 0 15px;'>{.overall.resolutionsLimitHigh}</td>
