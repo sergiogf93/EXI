bc513c7964fb2c677b57d83c37d4ef5f615d2c25
Alejandro De Maria Antolinos <demariaa@lindemaria.esrf.fr>
2017-02-02 15:16:44 +0100

    Added Synchrotron current in Beamline Parameters tab

--- min/exi.mx.js
+++ min/exi.mx.js
@@ -2999,7 +2999,7 @@ DataCollectionGrid.prototype.getColumns = function() {
                 catch(e){}
                 /** For Phasing */
                 
-                if (data.phasingStepType) {
+                if (data.phasingStepType) { 
                     var phasingSteps = data.phasingStepType.split(",");
                     data.phasingStepLength = phasingSteps.length;
                 
@@ -3013,6 +3013,7 @@ DataCollectionGrid.prototype.getColumns = function() {
 
                 /** Image quality indicator **/
                 data.indicator = EXI.getDataAdapter().mx.dataCollection.getQualityIndicatorPlot(record.data.DataCollection_dataCollectionId);                              
+
 
                 
                 data.onlineresults = _this._getAutoprocessingStatistics(record.data);
