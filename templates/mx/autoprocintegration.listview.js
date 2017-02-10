6e6d43ecea711a6857cd56f7177f2bd4a380a90c
GONZALEZ Sergio <SGONZALE@esrf.fr>
2017-02-10 14:31:18 +0100

    Format issues in energyScanGrid and Fluorescence

--- templates/mx/autoprocintegration.listview.js
+++ templates/mx/autoprocintegration.listview.js
@@ -1,4 +1,4 @@
 <table> 
-	<tr><td>Program:</td><td style='color:#207a7a;font-weight:bold;'>{.v_datacollection_processingPrograms}</td></tr>
-	<tr><td>Space group:</td><td>{.v_datacollection_summary_phasing_autoproc_space_group}</td></tr>	
+	<tr><td style='color:#207a7a;font-weight:bold;'>{.v_datacollection_processingPrograms}</td></tr>
+	<tr><td>{.v_datacollection_summary_phasing_autoproc_space_group}</td></tr>	
 </table>
--- templates/mx/datacollectionsummary/unitcell.autoproc.mxdatacollectiongrid.template.js
+++ templates/mx/datacollectionsummary/unitcell.autoproc.mxdatacollectiongrid.template.js
@@ -1,33 +1,33 @@
-   <table class='table-sm table table-striped'>
-                <thead>   
-                <tr>
-                    <th style='padding:0 15px 0 15px;font-weight: normal;'>Cell A</th>
-                    <th style='padding:0 15px 0 15px;font-weight: normal;'>Cell B</th>
-                    <th style='padding:0 15px 0 15px;font-weight: normal;'>Cell C</th>
-                    
-                </tr>
-                </thead>
-             <tbody>
-            
-                <tr>
-                    <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="overall.cell_a" decimals=2 intIfPossible="true" /}</td>
-                    <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="overall.cell_b" decimals=2 intIfPossible="true" /}</td>
-                    <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="overall.cell_c" decimals=2 intIfPossible="true" /}</td>
-                    
-                </tr>
-         
-                <tr>
-                    <th  style='padding:0 15px 0 15px;font-weight: normal;'>Alpha</th>
-                    <th style='padding:0 15px 0 15px;font-weight: normal;'>Beta</th>
-                    <th style='padding:0 15px 0 15px;font-weight: normal;'>Gamma</th>
-                    
-                </tr>
-               
-                <tr>
-                    <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="overall.cell_alpha" decimals=2 intIfPossible="true" /}</td>
-                    <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="overall.cell_beta" decimals=2 intIfPossible="true" /}</td>
-                    <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="overall.cell_gamma" decimals=2 intIfPossible="true" /}</td>
-                    
-                </tr>
-                 </tbody>
-         </table>
+<table class='table-sm table table-striped align-middle-header align-middle-rows'>
+    <thead>   
+    <tr>
+        <th style='padding:0 15px 0 15px;font-weight: normal;'>a</th>
+        <th style='padding:0 15px 0 15px;font-weight: normal;'>b</th>
+        <th style='padding:0 15px 0 15px;font-weight: normal;'>c</th>
+        
+    </tr>
+    </thead>
+    <tbody>
+
+    <tr>
+        <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="overall.cell_a" decimals=2 intIfPossible="true" /} &#197;</td>
+        <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="overall.cell_b" decimals=2 intIfPossible="true" /} &#197;</td>
+        <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="overall.cell_c" decimals=2 intIfPossible="true" /} &#197;</td>
+        
+    </tr>
+
+    <tr>
+        <td  style='padding:0 15px 0 15px;font-weight: normal;'>&#945;</td>
+        <td style='padding:0 15px 0 15px;font-weight: normal;'>&#946;</td>
+        <td style='padding:0 15px 0 15px;font-weight: normal;'>&#947;</td>
+        
+    </tr>
+    
+    <tr>
+        <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="overall.cell_alpha" decimals=2 intIfPossible="true" /} &deg;</td>
+        <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="overall.cell_beta" decimals=2 intIfPossible="true" /} &deg;</td>
+        <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="overall.cell_gamma" decimals=2 intIfPossible="true" /} &deg;</td>
+        
+    </tr>
+        </tbody>
+</table>
