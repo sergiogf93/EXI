71081142a3c193e2eec1366bc2675c566ebb8b93
Alejandro De Maria Antolinos <demariaa@lindemaria.esrf.fr>
2017-02-14 18:05:05 +0100

    If 0 data collection the badge is not displayed

--- templates/mx/session.grid.mx.datacollection.header.template.js
+++ templates/mx/session.grid.mx.datacollection.header.template.js
@@ -7,22 +7,27 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-md-3" style="padding:0px;">
+        <div class="col-md-2" style="padding:0px;">
             <div style="text-align:center;border-right:1px solid #ccc">
-                En / XRF
+                En
             </div>
         </div>
-        <div class="col-md-3" style="padding:0px;">
+         <div class="col-md-2" style="padding:0px;">
+            <div style="text-align:center;border-right:1px solid #ccc">
+                XRF
+            </div>
+        </div>
+        <div class="col-md-2" style="padding:0px;">
             <div style="text-align:center;border-right:1px solid #ccc">
                 Samples
             </div>
         </div>
-        <div class="col-md-3" style="padding:0px;">
+        <div class="col-md-2" style="padding:0px;">
             <div style="text-align:center;border-right:1px solid #ccc">
                 Test
             </div>
         </div>
-        <div class="col-md-3" style="padding:0px;">
+        <div class="col-md-2" style="padding:0px;">
             <div style="text-align:center;border-right:1px solid #ccc">
                 Collects
             </div>
