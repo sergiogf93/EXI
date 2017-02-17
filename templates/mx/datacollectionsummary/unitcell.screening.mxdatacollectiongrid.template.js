7606da7307c5d1bbbb59abb1fa3e5a8172db208e
Olof Svensson <svensson@esrf.fr>
2017-02-17 15:20:19 +0100

    Issue #122: Cell parameters in characterisation results

--- templates/mx/datacollectionsummary/unitcell.screening.mxdatacollectiongrid.template.js
+++ templates/mx/datacollectionsummary/unitcell.screening.mxdatacollectiongrid.template.js
@@ -1,32 +1,26 @@
-   <table class='table-sm table table-striped'>
-                <thead>   
-                <tr>
-                    <th  style='padding:0 15px 0 15px;'>Cell A</th>
-                    <th style='padding:0 15px 0 15px;'>Cell B</th>
-                    <th style='padding:0 15px 0 15px;'>Cell C</th>
-                </tr>
-                </thead>
-             <tbody>
-            
-                <tr>
-                    <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="ScreeningOutputLattice_unitCell_a" decimals=2 /}</td>
-                    <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="ScreeningOutputLattice_unitCell_b" decimals=2 /}</td>
-                    <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="ScreeningOutputLattice_unitCell_c" decimals=2 /}</td>
-                    
-                </tr>
-         
-                <tr>
-                    <th  style='padding:0 15px 0 15px;'>Alpha</th>
-                    <th style='padding:0 15px 0 15px;'>Beta</th>
-                    <th style='padding:0 15px 0 15px;'>Gamma</th>
-                    
-                </tr>
-               
-                <tr>
-                    <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="ScreeningOutputLattice_unitCell_alpha" decimals=2 /}</td>
-                    <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="ScreeningOutputLattice_unitCell_beta" decimals=2 /}</td>
-                    <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="ScreeningOutputLattice_unitCell_gamma" decimals=2 /}</td>
-                    
-                </tr>
-                 </tbody>
-         </table>
+<table class='table-sm table table-striped align-middle-header align-middle-rows'>
+    <thead>   
+        <tr>
+            <th style='padding:0 15px 0 15px;font-weight: normal;'>a</th>
+            <th style='padding:0 15px 0 15px;font-weight: normal;'>b</th>
+            <th style='padding:0 15px 0 15px;font-weight: normal;'>c</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="ScreeningOutputLattice_unitCell_a" decimals=2 /}</td>
+            <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="ScreeningOutputLattice_unitCell_b" decimals=2 /}</td>
+            <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="ScreeningOutputLattice_unitCell_c" decimals=2 /}</td>
+        </tr>
+        <tr>
+            <td style='padding:0 15px 0 15px;font-weight: normal;'>&#945;</td>
+            <td style='padding:0 15px 0 15px;font-weight: normal;'>&#946;</td>
+            <td style='padding:0 15px 0 15px;font-weight: normal;'>&#947;</td>
+        </tr>
+        <tr>
+            <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="ScreeningOutputLattice_unitCell_alpha" decimals=2 /}</td>
+            <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="ScreeningOutputLattice_unitCell_beta" decimals=2 /}</td>
+            <td class='column_parameter_value' style='padding:0 15px 0 15px;'>{@decimal key="ScreeningOutputLattice_unitCell_gamma" decimals=2 /}</td>
+        </tr>
+    </tbody>
+</table>
