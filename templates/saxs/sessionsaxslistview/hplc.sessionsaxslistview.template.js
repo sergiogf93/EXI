40748fc0fd27a229df2604db55793e77ab14ef74
GONZALEZ Sergio <SGONZALE@esrf.fr>
2017-03-29 13:32:01 +0200

    Button displayed

--- templates/saxs/sessionsaxslistview/hplc.sessionsaxslistview.template.js
+++ templates/saxs/sessionsaxslistview/hplc.sessionsaxslistview.template.js
@@ -1,23 +1,24 @@
- 
-        
 <kbd style='background-color:#AC58FA;font-size:12px;'>{.experimentType}</kbd>
-        <br />
-        {.creationDate}
-        <br />
-        {.name}
-        <br />
-        <table style='margin-left:10px;'>           
-            <tr>
-                <td  style="height:10px;">
-                Subtractions
-                </td>
-                <td style='padding-left:10px;'>
-                <div class="meter-wrap">
-                        <div class="meter-value" style="background-color: #99CC00 ; width: {.subtracted}%;">
-                            <div class="meter-text">{.dataCollectionDoneCount} of {.dataCollectionCount}</div>
-                        </div>
+<a href="#">
+    <span class="glyphicon glyphicon-download" style="margin-left:5px"></span>
+</a>
+<br />
+{.creationDate}
+<br />
+{.name}
+<br />
+<table style='margin-left:10px;'>           
+    <tr>
+        <td  style="height:10px;">
+        Subtractions
+        </td>
+        <td style='padding-left:10px;'>
+        <div class="meter-wrap">
+                <div class="meter-value" style="background-color: #99CC00 ; width: {.subtracted}%;">
+                    <div class="meter-text">{.dataCollectionDoneCount} of {.dataCollectionCount}</div>
                 </div>
-                </td>
-            </tr>
-        
-        </table>
+        </div>
+        </td>
+    </tr>
+
+</table>
--- templates/saxs/sessionsaxslistview/static.sessionsaxslistview.template.js
+++ templates/saxs/sessionsaxslistview/static.sessionsaxslistview.template.js
@@ -6,47 +6,50 @@
      <kbd style='background-color:#2E9AFE;font-size:12px;'>{.experimentType}</kbd>
    
 {/eq} 
-        <br />
-        {.creationDate}
-        <br />
-        {.name}
-        <br />
-        <table style='margin-left:10px;'>
-            <tr>
-                <td  style="height:10px;">
-                Samples
-                </td>
-                <td style='padding-left:10px;'>
-                <div class="meter-wrap">
-                        <div class="meter-value" style="background-color: #99CC00 ; width: {.measured}%;">
-                            <div class="meter-text">{.measurementDoneCount} of {.measurementCount}</div>
-                        </div>
+<a href="#">
+    <span class="glyphicon glyphicon-download"></span>
+</a>
+<br />
+{.creationDate}
+<br />
+{.name}
+<br />
+<table style='margin-left:10px;'>
+    <tr>
+        <td  style="height:10px;">
+        Samples
+        </td>
+        <td style='padding-left:10px;'>
+        <div class="meter-wrap">
+                <div class="meter-value" style="background-color: #99CC00 ; width: {.measured}%;">
+                    <div class="meter-text">{.measurementDoneCount} of {.measurementCount}</div>
                 </div>
-                </td>
-            </tr>
-            <tr>
-                <td  style="height:10px;">
-                Averages
-                </td>
-                <td style='padding-left:10px;'>
-                <div class="meter-wrap">
-                        <div class="meter-value" style="background-color: #99CC00 ; width: {.averaged}%;">
-                            <div class="meter-text">{.measurementAveragedCount} of {.measurementCount}</div>
-                        </div>
+        </div>
+        </td>
+    </tr>
+    <tr>
+        <td  style="height:10px;">
+        Averages
+        </td>
+        <td style='padding-left:10px;'>
+        <div class="meter-wrap">
+                <div class="meter-value" style="background-color: #99CC00 ; width: {.averaged}%;">
+                    <div class="meter-text">{.measurementAveragedCount} of {.measurementCount}</div>
                 </div>
-                </td>
-            </tr>
-            <tr>
-                <td  style="height:10px;">
-                Subtractions
-                </td>
-                <td style='padding-left:10px;'>
-                <div class="meter-wrap">
-                        <div class="meter-value" style="background-color: #99CC00 ; width: {.subtracted}%;">
-                            <div class="meter-text">{.dataCollectionDoneCount} of {.dataCollectionCount}</div>
-                        </div>
+        </div>
+        </td>
+    </tr>
+    <tr>
+        <td  style="height:10px;">
+        Subtractions
+        </td>
+        <td style='padding-left:10px;'>
+        <div class="meter-wrap">
+                <div class="meter-value" style="background-color: #99CC00 ; width: {.subtracted}%;">
+                    <div class="meter-text">{.dataCollectionDoneCount} of {.dataCollectionCount}</div>
                 </div>
-                </td>
-            </tr>
-        
-        </table>
+        </div>
+        </td>
+    </tr>
+
+</table>
